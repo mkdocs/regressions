c8b06abaaf21d695020485672e1b6ce950dd70db
Oleh Prypin <oleh@pryp.in>
2023-11-19 17:20:07 +0100

    Switch to mkdocs-get-deps

--- populate_projects.sh
+++ populate_projects.sh
@@ -21,7 +21,7 @@ for d in */; do (
   commit=$(curl -sfL "https://api.github.com/repos/$repo/commits?per_page=1&sha=$branch" | jq -r '.[0].sha')
   echo "https://github.com/$repo/blob/$branch/$mkdocs_yml" | tee /dev/stderr >project.txt
   echo "https://github.com/$repo/raw/$commit/$mkdocs_yml" >>project.txt
-  tail -1 project.txt | xargs curl -sfL | (mkdocs get-deps -f - || true) | grep . >requirements.in.new
+  tail -1 project.txt | xargs curl -sfL | (mkdocs-get-deps -f - || true) | grep . >requirements.in.new
   (grep -qE ' ' requirements.in || true) >>requirements.in.new
   mv requirements.in.new requirements.in
 ); done
