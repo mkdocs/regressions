b5d81aa391daf916d89cde38edcd2d81943ec0c5
Oleh Prypin <oleh@pryp.in>
2024-02-02 17:03:47 +0100

    Make the script detect renamed repositories

--- populate_projects.sh
+++ populate_projects.sh
@@ -4,9 +4,9 @@ set -e -u -o pipefail
 
 cd projects
 
-for d in */; do (
+for d in */; do
   d="${d%/}"
-  cd "$d"
+  pushd "$d"
   printf "%s -> " "$d" >&2
   if [[ -f 'project.txt' ]]; then
     [[ "$(head -1 'project.txt')" =~ ^https://github.com/([^/]+/[^/]+)/blob/([^/]+)/(.+)$ ]]
@@ -18,12 +18,20 @@ for d in */; do (
     branch=$(curl -sfL "https://api.github.com/repos/$repo" | jq -r '.default_branch')
     mkdocs_yml='mkdocs.yml'
   fi
-  commit=$(curl -sfL "https://api.github.com/repos/$repo/commits?per_page=1&sha=$branch" | jq -r '.[0].sha')
+  [[ "$(curl -sfL "https://api.github.com/repos/$repo/commits?per_page=1&sha=$branch" | jq -r '.[0].commit.url')" =~ ^https://api.github.com/repos/([^/]+/[^/]+)/git/commits/([0-9a-f]+)$ ]]
+  repo="${BASH_REMATCH[1]}"
+  commit="${BASH_REMATCH[2]}"
   echo "https://github.com/$repo/blob/$branch/$mkdocs_yml" | tee /dev/stderr >project.txt
   echo "https://github.com/$repo/raw/$commit/$mkdocs_yml" >>project.txt
   tail -1 project.txt | xargs curl -sfL | (mkdocs-get-deps -f - || true) | grep . >requirements.in.new
   (grep -qE ' ' requirements.in || true) >>requirements.in.new
   mv requirements.in.new requirements.in
-); done
+  popd
+  # Rename the directory in case the repository has been renamed
+  dir_name="${repo//\//--}"
+  if [[ "$d" != "$dir_name" ]]; then
+    mv "$d" "$dir_name"
+  fi
+done
 
 echo */requirements.in | xargs -t -n1 -P4 pip-compile -q --allow-unsafe --strip-extras --no-annotate --no-header -U
